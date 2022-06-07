2903e40475203b9de2b9fc242fcf9c8417243360
Jack Boucher <jboucher56@gmail.com>
2022-06-07 16:23:50 -0500

    added function Rating so that the stars coincide with Rating sort

--- books.js
+++ books.js
@@ -14,6 +14,9 @@ function renderBooks(filter) {
     books.sort((a,b) => b.rating - a.rating);
   }
 
+
+
+
   const booksHtml = books
     .map((book) => {
    return `<div class="book">
@@ -24,11 +27,7 @@ function renderBooks(filter) {
       ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
       <span class=>$${book.originalPrice.toFixed(2)}</span>
@@ -39,6 +38,18 @@ function renderBooks(filter) {
 
 
   booksWrapper.innerHTML = booksHtml;
+  }
+
+  function ratingsHTML(rating) {
+    let ratingHTML = "";
+    for (let i = 0; i < Math.floor(rating); ++i) {
+      ratingHTML += '<i class="fas fa-star"></i>\n'
+    }
+   
+    if (!Number.isInteger(rating)) {
+      ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+    }
+    return ratingHTML;
   }
 
   function filterBooks(event) {
